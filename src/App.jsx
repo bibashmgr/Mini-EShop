5aa535c9cc9f3458311f2f487bd45bc6a53e0a9e
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2021-12-31 22:27:11 +0545

    Setting-up repo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// scss
+import './App.scss';
+
+// components
+import Navbar from './components/Navbar';
+import Products from './components/Products';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Products />
+    </>
+  );
+}
+
+export default App;
