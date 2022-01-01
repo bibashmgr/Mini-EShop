b7b1554dd44a22717ea7c4c95a86b24e1c885fbd
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-01-01 10:56:58 +0545

    Adding pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// components
+import Products from '../components/Products';
+
+const Home = () => {
+    return (
+        <>
+            <Products />
+        </>
+    )
+}
+
+export default Home
